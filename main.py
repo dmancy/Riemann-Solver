Correct computation of speed waves

main.py
@@ -6,17 +6,12 @@ from State import State
 from Solution import Solution
 
 
+#Solve of the Riemann Problem
+Riemann_problem = Riemann_Computation(1., -2., 0.4, 1., 2., 0.4, 1.43)
 
-Riemann_problem = Riemann_Computation(5.81, 0, 5*10**5, 1.16, 0, 100*10**3, 1.4)
+t = 0.04
+N = 1000
+X = [-.5 + 1./N * (i - 0.5) for i in range(1, N+1)]
+Y = [Solution(Riemann_problem, x/t).p for x in X] 
 
-State = Solution(Riemann_problem, 487.1)
-
-t = 3*10**-3
-X = np.linspace(-2,2,1000)
-Y = [Solution(Riemann_problem,x/t).u for x in X] 
-
-plt.plot(X,Y)
-plt.grid()
-plt.xlabel("x")
-plt.show()
-
+Riemann_problem.plot_time(X, t)